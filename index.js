846b1c578d2fff552ff08bf30794061f78edc7bf
Taylor Williams <taylor@thewilliams.net>
2019-02-05 02:53:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers){
+  return drivers.map(function(driver){
+    const bothNames = driver.name.split(" ")
+    return Object.assign({}, {}, {
+      firstName:bothNames[0]
+      lastName:bothNames[1]
+    })
+  }
+}
