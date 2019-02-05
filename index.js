847774cb4077b4f2902c59df0bf7a36dcfb03afa
Taylor Williams <taylor@thewilliams.net>
2019-02-05 02:55:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function lowerCaseDrivers(drivers){
   return drivers.map(function(driver){
     const bothNames = driver.name.split(" ")
     return Object.assign({}, {}, {
-      firstName:bothNames[0].toLowerCase()
+      firstName:bothNames[0].toLowerCase(),
       lastName:bothNames[1].toLowerCase()
     })
   }
